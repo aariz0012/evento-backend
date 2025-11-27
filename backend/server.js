278abaef7290f4ce08690c7908f632f0a93e6c90
aariz0012 <185970087+aariz0012@users.noreply.github.com>
2025-11-27 12:58:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 const allowedOrigins = [
   'https://venuity.netlify.app',
   'https://localhost:3001',
-  'http://localhost:3001'
+  'http://localhost:3000'
 ];
 
 const corsOptions = {
