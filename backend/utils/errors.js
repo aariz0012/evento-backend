7c2d9188b914f4b3b78d3f6d35f509d12866019e
aariz0012 <185970087+aariz0012@users.noreply.github.com>
2025-12-08 16:42:33 +0530

    Create errors.js

---
+++ backend/utils/errors.js
@@ -0,0 +1,42 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class BadRequestError extends AppError {
+  constructor(message = 'Bad Request') {
+    super(message, 400);
+  }
+}
+
+class NotFoundError extends AppError {
+  constructor(message = 'Resource not found') {
+    super(message, 404);
+  }
+}
+
+class UnauthorizedError extends AppError {
+  constructor(message = 'Not authorized to access this route') {
+    super(message, 401);
+  }
+}
+
+class ForbiddenError extends AppError {
+  constructor(message = 'Forbidden') {
+    super(message, 403);
+  }
+}
+
+module.exports = {
+  AppError,
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+  ForbiddenError
+};
