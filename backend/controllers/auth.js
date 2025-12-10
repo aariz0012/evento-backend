27e2d44e7cf95ae435a10cbc14dfcfcb333f10a2
aariz0012 <185970087+aariz0012@users.noreply.github.com>
2025-12-10 15:09:33 +0530

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -430,28 +430,3 @@ exports.logout = async (req, res) => {
   });
 };
 
-// Helper function to get token from model, create cookie and send response
-const sendTokenResponse = (model, statusCode, res, isHost) => {
-  // Create token
-  const token = model.getSignedJwtToken();
-
-  const options = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true
-  };
-
-  if (process.env.NODE_ENV === 'production') {
-    options.secure = true;
-  }
-
-  res
-    .status(statusCode)
-    .cookie('token', token, options)
-    .json({
-      success: true,
-      token,
-      isHost
-    });
-};
